afe0dde47f04fa95258172992d5a87b60a8dd29f
gooosie <2375736523@qq.com>
2017-10-24 01:32:30 -0400

    feat(setting): config deply

--- gooosieblog/settings.py
+++ gooosieblog/settings.py
@@ -23,12 +23,12 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = os.getenv('DJANGO_SEC_KEY', default=None)
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = False 
 
 ALLOWED_HOSTS = [
-    '127.0.0.1', 
-    'localhost ', 
-    '.gooosie.com'
+    'localhost',
+    '127.0.0.1',
+    'gooosie.com',
 ]
 
 
@@ -125,7 +125,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static'),
-]
+STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
